1152df8ea790e958b0d6e7e03c97cf02de3061fc
UnluckySnoop <159040689+UnluckySnoop@users.noreply.github.com>
2025-01-20 10:39:33 +0100

    Add files via upload

---
+++ venv_practices.py
@@ -0,0 +1,10 @@
+""" 
+python -m venv .venv
+pip list
+pip uninstall ...
+pip install -upgrade ...
+
+pip freeze > requirements.txt
+pip install -r requirements.txt --> recréer l'environnement ailleurs.
+
+"""
